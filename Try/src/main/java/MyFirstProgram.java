06197480c4a371b6b36ae5e422398ae6ef419e0e
Sergey Golik <goliksa@gmail.com>
2020-02-28 07:57:13 +0300

    Добавил метод к классам и подредактировал вывод на экран

--- Try/src/main/java/MyFirstProgram.java
+++ Try/src/main/java/MyFirstProgram.java
@@ -3,12 +3,20 @@ public class MyFirstProgram {
   public static void main(String[] args) {
 
     System.out.println("Hello, world!");
+    System.out.println("");
+
     print("Serg");
+    System.out.println();
+
+    printAreas();
   }
 
   public static void print(String name) {
 
     System.out.println("Hello, " + name);
+  }
+
+  public static void printAreas() {
 
     Square s = new Square(5);
     Square s2 = new Square(10);
@@ -16,21 +24,12 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(4, 7);
     Rectangle r2 = new Rectangle(2, 2);
 
-    System.out.println("Площадь квадрата со сторонй " + s.l + " = " + area(s));
-    System.out.println("Площадь квадрата со сторонй " + s2.l + " = " + area(s2));
+    System.out.println("Площадь квадрата со сторонй " + s.l + " = " + s.area());
+    System.out.println("Площадь квадрата со сторонй " + s2.l + " = " + s2.area());
+    System.out.println("");
 
-    System.out.println("Пдощадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
-    System.out.println("Пдощадь прямоугольника со сторонами " + r2.a + " и " + r2.b + " = " + area(r2));
-
+    System.out.println("Пдощадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    System.out.println("Пдощадь прямоугольника со сторонами " + r2.a + " и " + r2.b + " = " + r2.area());
   }
 
-  public static double area(Square n) {
-
-    return n.l * n.l;
-  }
-
-  public static double area(Rectangle u) {
-
-    return u.a * u.b;
-  }
 }
--- Try/src/main/java/Square.java
+++ Try/src/main/java/Square.java
@@ -1,8 +1,14 @@
 public class Square {
-  double l;
 
-  public Square (double l) {
+  double l; // поле
+
+  public Square (double l) { // конструктор, для приема аргументов к классу
 
     this.l = l;
+  }
+
+  public double area() {  // метод - функция класса
+
+    return this.l * this.l;
   }
 }
