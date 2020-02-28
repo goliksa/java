271f398694b000388d7d0976209acc12dcd304fb
Sergey Golik <goliksa@gmail.com>
2020-02-28 09:24:36 +0300

    поменял координату Х для первой точки

--- Try/src/main/java/distance/Distance.java
+++ Try/src/main/java/distance/Distance.java
@@ -4,7 +4,7 @@ public class Distance {
 
   public static void main(String[] args) {
 
-    Point one = new Point(1, 2);
+    Point one = new Point(-1, 2);
     Point two = new Point(5, 5);
 
     System.out.println("Расстояние между точками А(" + one.x + ", " + one.y + ") и Б("+ two.x + ", " + two.y + ") = " + distance(one, two));
